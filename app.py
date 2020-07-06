5726f9b35e76777eeebe72be6798178985147d08
John-LiuJ <1603759794@qq.com>
2020-07-06 21:30:56 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import click
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect
 from flask import url_for
 from flask_sqlalchemy import SQLAlchemy
 
@@ -72,11 +72,46 @@ def inject_user():
 def page_not_found(e):
     return render_template('404.html'), 404
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        year = request.form.get('year')
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input.')
+            return redirect(url_for('index'))
+        movie = Movie(title=title, year=year)
+        db.session.add(movie)
+        db.session.commit()
+        flash('Item created.')
+        return redirect(url_for('index'))
+    user = User.query.first()
     movies = Movie.query.all()
-    return render_template('index.html', movies=movies)
+    return render_template('index.html', user=user, movies=movies)
 
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        if not title or not year or len(year) > 4 or len(title) > 60:
+            flash('Invalid input')
+            return redirect(url_for('edit', movie_id=movie_id))
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('Item updated.')
+        return redirect(url_for('index'))
+    return render_template('edit.html', movie=movie)
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('Item deleted.')
+    return redirect(url_for('index'))
 #
 # @app.route('/')
 # def hello():
